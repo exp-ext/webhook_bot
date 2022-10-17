6d31feac7b977e4e3e60c78d9089618ddab3fe0d
exp-ext <ext77@yandex.ru>
2022-10-17 13:25:03 +0300

    add

--- settings.py
+++ settings.py
@@ -4,34 +4,23 @@ import sys
 
 from telebot import TeleBot
 
-# токен телеграмм бота, получить можно
-# https://tlgg.ru/BotFather
+
 TOKEN = os.getenv('TOKEN')
 
-# ID telegram семейного группового чата
 CHAT_ID = os.getenv('CHAT_ID')
 
-# ID на сайте предоставления прогноза погоды
-# https://home.openweathermap.org/api_keys
 OW_API_ID = os.getenv('OW_API_ID')
 
-# ID на https://yandex.ru/dev/maps/geocoder/
 YANDEX_GEO_API = os.getenv('YANDEX_GEO_API')
 
-# ID детей для предоставления только детских анекдотов
 ID_CHILDREN = list(os.getenv('ID_CHILDREN').split())
 
-# ID admin in Telegram, куда будут приходить уведомлеия о пропуске времени
-# и прочая информация
 ID_ADMIN = os.getenv('ID_ADMIN')
 
-# домен webhook
 DOMEN = os.getenv('DOMEN')
 
-# подключение к базе данных postgres
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-# токен Яндекс Практикум
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 
 bot = TeleBot(TOKEN)
@@ -50,7 +39,6 @@ def check_tokens():
     """Проверка доступности переменных среды.."""
     env_vars = {
         'TOKEN': TOKEN,
-        'CHAT_ID': CHAT_ID,
         'OW_API_ID': OW_API_ID,
         'ID_ADMIN': ID_ADMIN,
         'DOMEN': DOMEN,
